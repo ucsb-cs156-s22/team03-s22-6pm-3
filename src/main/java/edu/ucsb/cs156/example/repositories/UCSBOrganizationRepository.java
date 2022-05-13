cc11b712db490068c18bb34cad8fcfd6ba3bc317
Zack Marks <zmarks@ucsb.edu>
2022-05-12 17:08:56 -0700

    zm - bringing in the files from team02 into this repo. no edits. just moving files over. organization files. files ran properly in team02

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -0,0 +1,16 @@
+//UCSBOrganization
+//UCSBOrganizationRepository
+//Directly copied from team02
+
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.UCSBOrganization;
+
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String> {
+} 
