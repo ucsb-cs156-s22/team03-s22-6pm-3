24e0dbf41e1741c003b721b2b107f28054ef8dd6
Ayush Shah <ayushshah@ucsb.edu>
2022-05-17 14:21:40 -0700

    as- Updated Menu Item so that the table works

--- frontend/src/main/pages/UCBSDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
+++ frontend/src/main/pages/UCBSDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
@@ -1,14 +1,25 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
+ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import DiningCommonsMenuItemTable from 'main/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable';
+import { useCurrentUser } from 'main/utils/currentUser';
 export default function UCSBDiningCommonsMenuItemIndexPage() {
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>UCSB Dining Commons Menu Item</h1>
-        <p>
-          This is where the index page will go
-        </p>
-      </div>
-    </BasicLayout>
-  )
+
+    const currentUser = useCurrentUser();
+    const { data: UCSBDiningCommonsMenuItem, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/UCSBDiningCommonsMenuItem/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/UCSBDiningCommonsMenuItem/all" },
+      []
+    );
+    return (
+        <BasicLayout>
+          <div className="pt-2">
+            <h1>UCSB Dining Commons Menu Item Table</h1>
+            <DiningCommonsMenuItemTable UCSBDiningCommonsMenuItem={UCSBDiningCommonsMenuItem} currentUser={currentUser} />
+          </div>
+        </BasicLayout>
+      )
 }
