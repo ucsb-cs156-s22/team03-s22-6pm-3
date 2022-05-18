3486ba8cc368083df0b665970fe0cef026da1531
Yvonneliu0201 <yvonneliu@ucsb.edu>
2022-05-18 08:37:43 -0700

    yl - fixed javascript syntax test fail

--- frontend/src/tests/pages/Reviews/ReviewsIndexPage.test.js
+++ frontend/src/tests/pages/Reviews/ReviewsIndexPage.test.js
@@ -1,5 +1,5 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
-//import { render } from "@testing-library/react";
+//import { fireEvent, render, waitFor } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import ReviewsIndexPage from "main/pages/Reviews/ReviewsIndexPage";
