754f910d5ce45d9739738db2a4568bd98830cf54
Stanley Su <stanleysu@ucsb.edu>
2022-05-17 20:40:34 -0700

    ss - added delete column to the articletable

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -1,10 +1,22 @@
-import OurTable from "main/components/OurTable";
-// import { useBackendMutation } from "main/utils/useBackend";
-// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
 // import { useNavigate } from "react-router-dom";
-//import { hasRole } from "main/utils/currentUser";
+import { hasRole } from "main/utils/currentUser";
 
-export default function ArticlesTable({ articles, _currentUser }) {
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/Article",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
+
+
+export default function ArticlesTable({ articles, currentUser }) {
 
     //const navigate = useNavigate();
 
@@ -13,18 +25,22 @@ export default function ArticlesTable({ articles, _currentUser }) {
     //}
 
     // Stryker disable all : hard to test for query caching
-    //const deleteMutation = useBackendMutation(
-    //    cellToAxiosParamsDelete,
-    //    { onSuccess: onDeleteSuccess },
-    //    ["/api/ucsbdates/all"]
-    //);
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/Article/all"]
+    );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
     
     
     const columns = [
+        {
+            Header: 'Id',
+            accessor: 'id',
+        },
         {
             Header: 'Title',
             accessor: 'title', 
@@ -46,19 +62,20 @@ export default function ArticlesTable({ articles, _currentUser }) {
             accessor: 'dateAdded',
         }
     ];
-    /*
+    
+    const testid = "ArticlesTable";
+
     const columnsIfAdmin = [
         ...columns,
-        ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
-        ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
+        //ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
     ];
-    */
-    //const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
-    const columnsToDisplay = columns;
     return <OurTable
         data={articles}
         columns={columnsToDisplay}
-        testid={"ArticlesTable"}
+        testid={testid}
     />;
 };
