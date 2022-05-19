4b67ceba9abe9f2053419e3984bff1cb371e2328
Ayush Shah <ayushshah@ucsb.edu>
2022-05-18 18:19:46 -0700

    as- added placeholder and passes all test

---
+++ frontend/src/main/pages/UCBSDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function UCSBDiningCommonsMenuItemIndexPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>UCSB Dining Commons Menu Item</h1>
+        <p>
+          This is where the index page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+} 
