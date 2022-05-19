099fd3040ec3caeb89a4ba742e6a757184f531fd
Zack Marks <zmarks@ucsb.edu>
2022-05-19 03:03:47 -0700

    more testing, eslint, story items

---
+++ frontend/src/stories/pages/UCSBOrganization/UCSBOrganizationIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import UCSBOrganizationIndexPage from "main/pages/UCSBOrganization/UCSBOrganizationIndexPage";
+
+export default {
+    title: 'pages/UCSBOrganization/UCSBOrganizationIndexPage',
+    component: UCSBOrganizationIndexPage
+};
+
+const Template = () => <ReviewsIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+
