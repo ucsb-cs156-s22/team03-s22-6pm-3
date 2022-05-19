14516352e52f0adc62ff4f43c639d8b6e1ddf403
Yvonneliu0201 <yvonneliu@ucsb.edu>
2022-05-18 18:14:08 -0700

    yl - added review to storybook

---
+++ frontend/src/stories/pages/Reviews/ReviewsIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import ReviewsIndexPage from "main/pages/Reviews/ReviewsIndexPage";
+
+export default {
+    title: 'pages/Reviews/ReviewsIndexPage',
+    component: ReviewsIndexPage
+};
+
+const Template = () => <ReviewsIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+
