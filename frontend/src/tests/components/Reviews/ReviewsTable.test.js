0a789e867e947f7d5df61034d3f1f58394f0f945
Yvonneliu0201 <yvonneliu@ucsb.edu>
2022-05-18 18:24:14 -0700

    yl - altered ReviewsTable.test.js to include fixtures

--- frontend/src/tests/components/Reviews/ReviewsTable.test.js
+++ frontend/src/tests/components/Reviews/ReviewsTable.test.js
@@ -1,5 +1,5 @@
 import {  render } from "@testing-library/react";
-//import { diningCommonsFixtures } from "fixtures/diningCommonsFixtures";
+import { reviewsFixtures } from "fixtures/reviewsFixtures";
 import ReviewsTable from "main/components/Reviews/ReviewsTable";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -55,21 +55,14 @@ describe("ReviewsTable tests", () => {
     );
   });
 
-  test("w/o using fixtures: Has the expected column headers and content for adminUser", () => {
+  test("Has the expected column headers and content for adminUser", () => {
 
     const currentUser = currentUserFixtures.adminUser;
 
     const { getByText, getByTestId } = render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <ReviewsTable reviews={[{
-                "id": 0,
-                "itemId": 1,
-                "reviewerEmail": "y@ucsb.edu",
-                "stars": 3,
-                "dateReviewed": "2022-05-18T07:06:00",
-                "comments": "This is a test w/o using fixtures"
-            }]} currentUser={currentUser} />
+          <ReviewsTable reviews={reviewsFixtures.threeReviews} currentUser={currentUser} />
         </MemoryRouter>
       </QueryClientProvider>
 
@@ -90,8 +83,10 @@ describe("ReviewsTable tests", () => {
       expect(header).toBeInTheDocument();
     });
 
-    expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("0");
+    expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
     expect(getByTestId(`${testId}-cell-row-0-col-reviewerEmail`)).toHaveTextContent("y@ucsb.edu");
+    expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+    expect(getByTestId(`${testId}-cell-row-1-col-reviewerEmail`)).toHaveTextContent("e@ucsb.edu");
 
     // const editButton = getByTestId(`${testId}-cell-row-0-col-Edit-button`);
     // expect(editButton).toBeInTheDocument();
@@ -102,49 +97,6 @@ describe("ReviewsTable tests", () => {
     expect(deleteButton).toHaveClass("btn-danger");
 
   });
-
-//   test("Has the expected column headers and content for adminUser", () => {
-
-//     const currentUser = currentUserFixtures.adminUser;
-
-//     const { getByText, getByTestId } = render(
-//       <QueryClientProvider client={queryClient}>
-//         <MemoryRouter>
-//           <ReviewsTable diningCommons={diningCommonsFixtures.threeCommons} currentUser={currentUser} />
-//         </MemoryRouter>
-//       </QueryClientProvider>
-
-//     );
-
-
-//     const expectedHeaders = ['Code',  'Name', 'Sack Meal?','Takeout Meal?','Dining Cam?','Latitude','Longitude'];
-//     const expectedFields = ['code', 'name','hasSackMeal', 'hasTakeOutMeal','hasDiningCam','latitude','longitude'];
-//     const testId = "ReviewsTable";
-
-//     expectedHeaders.forEach((headerText) => {
-//       const header = getByText(headerText);
-//       expect(header).toBeInTheDocument();
-//     });
-
-//     expectedFields.forEach((field) => {
-//       const header = getByTestId(`${testId}-cell-row-0-col-${field}`);
-//       expect(header).toBeInTheDocument();
-//     });
-
-//     expect(getByTestId(`${testId}-cell-row-0-col-code`)).toHaveTextContent("de-la-guerra");
-//     expect(getByTestId(`${testId}-cell-row-1-col-code`)).toHaveTextContent("ortega");
-//     expect(getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("De La Guerra");
-//     expect(getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("Ortega");
-
-//     // const editButton = getByTestId(`${testId}-cell-row-0-col-Edit-button`);
-//     // expect(editButton).toBeInTheDocument();
-//     // expect(editButton).toHaveClass("btn-primary");
-
-//     const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
-//     expect(deleteButton).toBeInTheDocument();
-//     expect(deleteButton).toHaveClass("btn-danger");
-
-//   });
 
   // test("Edit button navigates to the edit page for admin user", async () => {
 
